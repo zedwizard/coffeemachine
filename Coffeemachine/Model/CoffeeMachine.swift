569cfcdc53b5788ba7e661f377650da070ab5bd8
zed <jahkushkin@gmail.com>
2019-09-19 08:08:55 +0300

    Initial Commit

---
+++ Coffeemachine/Model/CoffeeMachine.swift
@@ -0,0 +1,82 @@
+//
+//  CoffeeMachine.swift
+//  Coffeemachine
+//
+//  Created by Zed on 9/16/19.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Drink {
+    let title: String
+    let water: Int
+    let coffeeBeans: Int
+    let milk: Int
+}
+class CoffeeMachine {
+    var coffeeBeans: Int
+    var water: Int
+    var milk: Int
+    var wasteContainer: Int
+    enum CoffeeDrink {
+        case espresso
+        case americano
+        case cappuccino
+        case latte
+        var drink: Drink {
+            switch self {
+            case .espresso: return Drink(title: "espresso", water: 30, coffeeBeans: 50, milk: 0)
+            case .americano: return Drink(title: "americano", water: 60, coffeeBeans: 50, milk: 0)
+            case .cappuccino: return Drink(title: "cappuccino", water: 10, coffeeBeans: 50, milk: 60)
+            case .latte: return Drink(title: "latte", water: 0, coffeeBeans: 30, milk: 70)
+            }
+        }
+        
+    }
+    init(coffeeBeans: Int, water: Int, milk: Int, wasteContainer: Int) {
+        self.coffeeBeans = coffeeBeans
+        self.water = water
+        self.milk = milk
+        self.wasteContainer = wasteContainer
+    }
+    func makeDrink(drink: CoffeeDrink) -> (message: String, ready: Bool) {
+        let currentDrink = drink.drink
+        if coffeeBeans < currentDrink.coffeeBeans {
+            return ("not enough coffee beans", ready: false)
+        } else if water < currentDrink.water {
+            return ("not enough water", ready: false)
+        } else if milk < currentDrink.milk {
+            return ("not enough milk", ready: false)
+        } else if wasteContainer >= 500 {
+            return ("waste beans container is full", ready: false)
+        } else {
+            coffeeBeans -= currentDrink.coffeeBeans
+            water -= currentDrink.water
+            milk -= currentDrink.milk
+            wasteContainer += currentDrink.coffeeBeans
+            
+            return ("Your \(currentDrink.title) is ready", ready: true)
+        }
+    }
+    func addCoffeeBeans() -> String {
+        coffeeBeans = 250
+        return "coffee beans was added"
+    }
+    func addWater() -> String {
+        water = 250
+        return "water was added"
+    }
+    func addMilk() -> String {
+        milk = 250
+        return "milk was added"
+    }
+    func cleanWasteContainer() -> String {
+        wasteContainer = 0
+        return "waste bean container was cleaned"
+    }
+    
+    
+}
---
+++ Coffeemachine/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Coffeemachine
+//
+//  Created by Zed on 9/5/19.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var espressoButton: UIButton!
+    @IBOutlet weak var americanoButton: UIButton!
+    @IBOutlet weak var cappuccinoButton: UIButton!
+    @IBOutlet weak var latteButton: UIButton!
+    @IBOutlet weak var addBeansButton: UIButton!
+    @IBOutlet weak var addWaterButton: UIButton!
+    @IBOutlet weak var addMilkButton: UIButton!
+    @IBOutlet weak var coffeeMachineDisplay: UILabel!
+    @IBOutlet weak var clearContainerButton: UIButton!
+    @IBOutlet weak var progressLine: UIProgressView!
+    
+    private let coffeeMachine = CoffeeMachine(coffeeBeans: 0, water: 0, milk: 0, wasteContainer: 0)
+    //    private let progress = Progress(totalUnitCount: 10)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        
+    }
+
+
+    @IBAction func espressoButtonAction(_ sender: UIButton) {
+        clearDisplay()
+        let message = coffeeMachine.makeDrink(drink: .espresso)
+        if message.ready == false {
+            coffeeMachineDisplay.text = message.message
+        } else if message.ready == true {
+            _ = showProgressBarAnimation()
+//            {
+//                didSet {
+//                    if animationEnded == true {
+//                        print("inside didset")
+                        coffeeMachineDisplay.text = message.message
+//                        progressLine.progress = 0
+//                    }
+//                }
+//            }
+        }
+    }
+    @IBAction func cappuccinoButtonAction(_ sender: UIButton) {
+        let message = coffeeMachine.makeDrink(drink: .cappuccino)
+        coffeeMachineDisplay.text = message.message
+    }
+    @IBAction func americanoButtonAction(_ sender: UIButton) {
+        let message = coffeeMachine.makeDrink(drink: .americano)
+        coffeeMachineDisplay.text = message.message
+    }
+    @IBAction func latteButtonAction(_ sender: UIButton) {
+        let message = coffeeMachine.makeDrink(drink: .latte)
+        coffeeMachineDisplay.text = message.message
+    }
+    @IBAction func addBeansButtonAction(_ sender: UIButton) {
+        coffeeMachineDisplay.text = coffeeMachine.addCoffeeBeans()
+    }
+    @IBAction func addWaterButtonAction(_ sender: UIButton) {
+        coffeeMachineDisplay.text = coffeeMachine.addWater()
+    }
+    @IBAction func addMilkButtonAction(_ sender: UIButton) {
+        coffeeMachineDisplay.text = coffeeMachine.addMilk()
+    }
+    @IBAction func clearContainerAction(_ sender: UIButton) {
+        coffeeMachineDisplay.text = coffeeMachine.cleanWasteContainer()
+    }
+    func clearDisplay() {
+        coffeeMachineDisplay.text = ""
+    }
+    func showProgressBarAnimation() -> Bool {
+        progressLine.progress = 0
+        UIView.animate(withDuration: 3, animations: {
+            self.progressLine.setProgress(1.0, animated: true)
+            
+        }) { (true) in
+            print("animation ended")
+        }
+
+        return true
+    }
+
+}
+
