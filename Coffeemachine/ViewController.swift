2b1bca0f6f2f6b1c076d8c452f381e26cf2d6549
zed <jahkushkin@gmail.com>
2019-09-23 18:48:01 +0300

    progress bar works, but with some delay, dunno why

--- Coffeemachine/ViewController.swift
+++ Coffeemachine/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressLine: UIProgressView!
     
     private let coffeeMachine = CoffeeMachine(coffeeBeans: 0, water: 0, milk: 0, wasteContainer: 0)
-    //    private let progress = Progress(totalUnitCount: 10)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,32 +34,22 @@ class ViewController: UIViewController {
     @IBAction func espressoButtonAction(_ sender: UIButton) {
         clearDisplay()
         let message = coffeeMachine.makeDrink(drink: .espresso)
-        if message.ready == false {
-            coffeeMachineDisplay.text = message.message
-        } else if message.ready == true {
-            _ = showProgressBarAnimation()
-//            {
-//                didSet {
-//                    if animationEnded == true {
-//                        print("inside didset")
-                        coffeeMachineDisplay.text = message.message
-//                        progressLine.progress = 0
-//                    }
-//                }
-//            }
-        }
+        showMessageAfterProgressBarAnimation(ready: message.ready, message: message.message)
     }
     @IBAction func cappuccinoButtonAction(_ sender: UIButton) {
+        clearDisplay()
         let message = coffeeMachine.makeDrink(drink: .cappuccino)
-        coffeeMachineDisplay.text = message.message
+        showMessageAfterProgressBarAnimation(ready: message.ready, message: message.message)
     }
     @IBAction func americanoButtonAction(_ sender: UIButton) {
+        clearDisplay()
         let message = coffeeMachine.makeDrink(drink: .americano)
-        coffeeMachineDisplay.text = message.message
+        showMessageAfterProgressBarAnimation(ready: message.ready, message: message.message)
     }
     @IBAction func latteButtonAction(_ sender: UIButton) {
+        clearDisplay()
         let message = coffeeMachine.makeDrink(drink: .latte)
-        coffeeMachineDisplay.text = message.message
+        showMessageAfterProgressBarAnimation(ready: message.ready, message: message.message)
     }
     @IBAction func addBeansButtonAction(_ sender: UIButton) {
         coffeeMachineDisplay.text = coffeeMachine.addCoffeeBeans()
@@ -76,15 +66,20 @@ class ViewController: UIViewController {
     func clearDisplay() {
         coffeeMachineDisplay.text = ""
     }
-    func showProgressBarAnimation() -> Bool {
-        progressLine.progress = 0
-        UIView.animate(withDuration: 3, animations: {
+    func showMessageAfterProgressBarAnimation(ready: Bool, message: String) {
+        if ready == false {
+            coffeeMachineDisplay.text = message
+        } else if ready == true {
+            CATransaction.setCompletionBlock { [weak self] in
+                print("before message: \(message)")
+                self?.coffeeMachineDisplay.text = message
+                self?.progressLine.progress = 0
+                print("after message: \(message)")
+            }
             self.progressLine.setProgress(1.0, animated: true)
-            
-        }) { (true) in
-            print("animation ended")
+            CATransaction.commit()
         }
-        return true
+        
     }
 
 }
